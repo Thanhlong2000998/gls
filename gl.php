94ec7ca3406da9d333b661ea6686672aa2dad4f5
Lê Thành Long <106485602+Thanhlong2000998@users.noreply.github.com>
2023-09-18 01:03:20 +0700

    update

--- gl.php
+++ gl.php
@@ -154,7 +154,7 @@ if ($nhan_coin["success"]== 200 ){
 	$object_id = $nhan_coin["data"]["object_id"];
 	$gio = date("H:i");
 $tt = $tt+1;
-echo "".$do." | ".$BBlue.$tt.$do." | ".$trang.$type_2.$do." | ".$BBlue."SUCCESS ".$do."|                             \n";}
+echo "".$do." | ".$BBlue.$tt.$do." | ".$luc.$gio.$do." | ".$trang.$type_2.$do." | ".$vang.$object_id.$do." | ".$BBlue."SUCCESS ".$do." | ".$BBlue.$tt.$do."\n";
 }else{
     $count = intval($ct);
     while ($count > 0)
